5045f6a3dfb6aa8911026815be7456a12c5e2543
Hejrong <981851750@qq.com>
2018-08-17 05:15:18 +0800

    feicuixiangmu

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// // 点击置顶
+// $(function (){
+//     $('.settop').on('click',function(){
+//         scrollTo(00,0);
+//     })
+// }());
+// $('span').on('click',function(){
+// 	this.style.display="none";
+// })
