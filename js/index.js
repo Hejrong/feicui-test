2bede890e0f0ab3dc2e24cf35884a20aaddbd570
Hejrong <981851750@qq.com>
2018-08-18 17:09:54 +0800

    wucigengxin

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-//点击播放视频
-var myVideo=document.getElementById("video1"),
-            video1 = document.getElementById('play'),
-            play = document.getElementsByClassName('play')[0],
-            play1 = document.getElementsByClassName('play')[1],
-            n = 0,
-            n1 = 0;
-        myVideo.onclick=function(){
-            n++;
-            if(n % 2 == 1){
-                this.play();
-                play.style.display='none';
-            }else{
-                this.pause();
-                play.style.display='inline-block';
-            }
-            myVideo.loop=true;
-        }
-        video1.onclick=function(){
-            n1++;
-            if(n1 % 2 == 1){
-                this.play();
-                play1.style.display='none';
-            }else{
-                this.pause();
-                play1.style.display='inline-block';
-            }
-            this.loop=true;
-        }
 // 点击图片跳二维码
 	var img = $('.con ul li .img'),
 		title = $('.con ul li .title'),
@@ -34,15 +5,6 @@ var myVideo=document.getElementById("video1"),
 		gb = $('.tiaozhaun .ew .gb span'),
 		len1 = title.length,
 		len2 = img.length;
-		// for (var i = 0; i <= len1; i++) {
-		// 	title[i].onclick=function(){tiaozhuan.classList.add("tz");}
-		// }
-		// for(var i = 0; i <= len2; i++){
-		// 	title[i].onclick=function(){tiaozhuan.classList.add("tz");}
-		// }
-		// gb.on('click', function(){
-		// 	tiaozhuan.classList.remove("tz");
-		// });
 		$('.con ul li ').find('.title').on('click', function(){
            $('#tiaozhuan').addClass("tz");
         });
