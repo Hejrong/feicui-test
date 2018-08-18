70374e3b7e2e3d0ee8d33771024a3e16cc8e5075
Hejrong <981851750@qq.com>
2018-08-18 16:12:29 +0800

    sicigengxin

--- js/index.js
+++ js/index.js
@@ -27,3 +27,28 @@ var myVideo=document.getElementById("video1"),
             }
             this.loop=true;
         }
+// 点击图片跳二维码
+	var img = $('.con ul li .img'),
+		title = $('.con ul li .title'),
+		tiaozhuan = document.getElementsByClassName('tiaozhuan')[0],
+		gb = $('.tiaozhaun .ew .gb span'),
+		len1 = title.length,
+		len2 = img.length;
+		// for (var i = 0; i <= len1; i++) {
+		// 	title[i].onclick=function(){tiaozhuan.classList.add("tz");}
+		// }
+		// for(var i = 0; i <= len2; i++){
+		// 	title[i].onclick=function(){tiaozhuan.classList.add("tz");}
+		// }
+		// gb.on('click', function(){
+		// 	tiaozhuan.classList.remove("tz");
+		// });
+		$('.con ul li ').find('.title').on('click', function(){
+           $('#tiaozhuan').addClass("tz");
+        });
+        $('.con ul li ').find('.img').on('click', function(){
+           $('#tiaozhuan').addClass("tz");
+        });
+        $('#gb').on('click', function(){
+        	$('#tiaozhuan').removeClass("tz");
+        })
