c6918efef6af11909a06faef83d7003c4d5466bb
Hejrong <981851750@qq.com>
2018-08-18 19:20:30 +0800

    liucigengxin

--- js/index.js
+++ js/index.js
@@ -1,52 +1,43 @@
-// 微信上播放
-function musicInWeixinHandler() {   
+// function autoPlayMusic() {
+//     // 自动播放音乐效果，解决浏览器或者APP自动播放问题
+//     function musicInBrowserHandler() {
+//         musicPlay(true);
+//         document.body.removeEventListener('touchstart', musicInBrowserHandler);
+//     }
+//     document.body.addEventListener('touchstart', musicInBrowserHandler);
 
-musicPlay(true);   
-
-document.addEventListener("WeixinJSBridgeReady", function () {   
-
-musicPlay(true);   
-
-}, false);   
-
-document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);   
-
-}   
-
-document.addEventListener('DOMContentLoaded', musicInWeixinHandler);   
-
-}   
-
-function musicPlay(isPlay) {   
-
-var media = document.getElementById('video1');   
-
-if (isPlay && media.paused) {   
-
-media.play();   
-
-}   
-
-if (!isPlay && !media.paused) {   
-
-media.pause();   
-
-}   
-
-}  
+//     // 自动播放音乐效果，解决微信自动播放问题
+//     function musicInWeixinHandler() {
+//         musicPlay(true);
+//         document.addEventListener("WeixinJSBridgeReady", function () {
+//             musicPlay(true);
+//         }, false);
+//         document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);
+//     }
+//     document.addEventListener('DOMContentLoaded', musicInWeixinHandler);
+// }
+// function musicPlay(isPlay) {
+//     var media = document.querySelector('#video1');
+//     if (isPlay && media.paused) {
+//         media.play();
+//     }
+//     if (!isPlay && !media.paused) {
+//         media.pause();
+//     }
+// }
 // 点击图片跳二维码
-	var img = $('.con ul li .img'),
-		title = $('.con ul li .title'),
-		tiaozhuan = document.getElementsByClassName('tiaozhuan')[0],
-		gb = $('.tiaozhaun .ew .gb span'),
-		len1 = title.length,
-		len2 = img.length;
-		$('.con ul li ').find('.title').on('click', function(){
-           $('#tiaozhuan').addClass("tz");
-        });
-        $('.con ul li ').find('.img').on('click', function(){
-           $('#tiaozhuan').addClass("tz");
-        });
-        $('#gb').on('click', function(){
-        	$('#tiaozhuan').removeClass("tz");
-        })
+var img = $('.con ul li .img'),
+  title = $('.con ul li .title'),
+  gb = $('.tiaozhaun .ew .gb span'),
+  len1 = title.length,
+  len2 = img.length;
+$('.con ul li ').find('.title').on('click', function () {
+  $('#tiaozhuan').addClass("tz");
+});
+$('.con ul li ').find('.img').on('click', function () {
+  $('#tiaozhuan').addClass("tz");
+});
+$('#gb').on('click', function () {
+  $('#tiaozhuan').removeClass("tz");
+});
+
